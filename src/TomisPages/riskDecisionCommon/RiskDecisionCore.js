737db19033c3c10e754660ac5cea4d8106c964b9
Greg <gtfullton@gmail.com>
2017-04-17 05:54:14 -0400

    Rename flex-column-pad css to internal.

--- src/TomisPages/riskDecisionCommon/RiskDecisionCore.js
+++ src/TomisPages/riskDecisionCommon/RiskDecisionCore.js
@@ -14,22 +14,22 @@ import MonthlyCalendarIcon from 'material-ui/svg-icons/notification/event-note';
 import SvgIconArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
 
 const initState = {
-    dataSource1: ['v1', 'v2', 'v3', 'v4', 'v5', 'v6'],
-    dataSource2: ['d1', 'd2', 'd3', 'd4', 'd5', 'd6'],
-    isExpanded: true,
-    demoDate: new Date(),
-    autoOk: false,
-    disableYearSelection: false
+  dataSource1: ['v1', 'v2', 'v3', 'v4', 'v5', 'v6'],
+  dataSource2: ['d1', 'd2', 'd3', 'd4', 'd5', 'd6'],
+  isExpanded: true,
+  demoDate: new Date(),
+  autoOk: false,
+  disableYearSelection: false
 };
 
 const defaultProps = {
-    isAccept: false,
-    isReject: false
+  isAccept: false,
+  isReject: false
 };
 
 const propTypes = {
-    isAccept: PropTypes.bool,
-    isReject: PropTypes.bool
+  isAccept: PropTypes.bool,
+  isReject: PropTypes.bool
 };
 
 const selectedBackgroundColor = 'rgba(0,0,0,0.27)';
@@ -39,88 +39,94 @@ const selectedLabelColor = 'rgba(0,0,0,0.87)';
 const unselectedLabelColor = 'rgba(0,0,0,0.54)';
 
 class RiskDecisionCore extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initState;
-        this.handleUpdateInput = this.handleUpdateInput.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = initState;
+    this.handleUpdateInput = this.handleUpdateInput.bind(this);
+  }
 
-    handleUpdateInput(value) {
-        this.setState(
-            {
-                // dataSource1: [value, value + value, value + value + value],
-                // dataSource2: [value, value + value, value + value + value]
-            }
-        );
-    }
+  handleUpdateInput(value) {
+    this.setState(
+      {
+        // dataSource1: [value, value + value, value + value + value],
+        // dataSource2: [value, value + value, value + value + value]
+      }
+    );
+  }
 
-    handleChangeDemoDate = (event, date) => {
-        this.setState({
-            demoDate: date
-        });
-    };
+  handleChangeDemoDate = (event, date) => {
+    this.setState({
+      demoDate: date
+    });
+  };
 
-    render() {
-        const { getBackgroundColorAccept, getLabelColorAccept, getBackgroundColorReject, getLabelColorReject, handleClickNameLov } = this;
-        const { riskDecisionVal } = this.props;
-        const { dataSource1, dataSource2 } = this.state;
-        return (
-            <div>
-                <HeaderNavAction actionBarPageTitle="Flight Planning" />
-                <Card expanded={true}>
-                    <CardHeader title="Risk Decision" actAsExpander={true} showExpandableButton={true} />
-                    <CardText expandable={true}>
-                        <div className="flex-row">
-                            <div>
-                                <label className="labeled-item">Flight Status (RA)*</label>
-                                <ToggleButtonGroup defaultSelected={riskDecisionVal}>
-                                    <ToggleButton value="ACCEPT" label="Accept" />
-                                    <ToggleButton value="REJECT" label="Reject" />
-                                </ToggleButtonGroup>
-                            </div>
-                            <div className="flex-1 flex-column-pad flex-row">
-                                <AutoComplete fullWidth={true} hintText="Choose Title" dataSource={dataSource2} onUpdateInput={this.handleUpdateInput} floatingLabelText="Title*" />
-                            </div>
-                            <div className="flex-1 flex-column-pad flex-row">
-                                <AutoComplete
-                                    fullWidth={true}
-                                    filter={AutoComplete.noFilter}
-                                    hintText="Choose Name"
-                                    openOnFocus={false}
-                                    dataSource={dataSource1}
-                                    onUpdateInput={this.handleUpdateInput}
-                                    floatingLabelText="Name*"
-                                />
-                            </div>
-                            <div className="flex-1 flex-column-pad flex-row">
-                                <DatePicker
-                                    className="flex-1"
-                                    container="inline"
-                                    fullWidth={true}
-                                    hintText={<span>Select Date </span>}
-                                    floatingLabelText="Date*"
-                                    defaultDate={this.state.demoDate}
-                                    onChange={this.handleChangeDemoDate}
-                                    mode="landscape"
-                                    firstDayOfWeek={0}
-                                    formatDate={date => date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear()}
-                                />
-                                {/* must use inline style for position on IconButton to override default */}
-                                <IconButton className="inline-icon" style={{ position: 'absolute' }}>
-                                    <MonthlyCalendarIcon />
-                                </IconButton>
-                            </div>
-                        </div>
-                        <div className="row-spacer-24">
-                            <h3>Attachments</h3>
-                            <img src="/images/fileUpload.png" />
-                        </div>
-                        {this.props.children}
-                    </CardText>
-                </Card>
+  render() {
+    const { getBackgroundColorAccept, getLabelColorAccept, getBackgroundColorReject, getLabelColorReject, handleClickNameLov } = this;
+    const { riskDecisionVal } = this.props;
+    const { dataSource1, dataSource2 } = this.state;
+    return (
+      <div>
+        <HeaderNavAction actionBarPageTitle="Flight Planning" />
+        <Card expanded={true}>
+          <CardHeader title="Risk Decision" actAsExpander={true} showExpandableButton={true} />
+          <CardText expandable={true}>
+            <div className="flex-row">
+              <div>
+                <label className="labeled-item">Flight Status (RA)*</label>
+                <ToggleButtonGroup defaultSelected={riskDecisionVal}>
+                  <ToggleButton value="ACCEPT" label="Accept" />
+                  <ToggleButton value="REJECT" label="Reject" />
+                </ToggleButtonGroup>
+              </div>
+              <div className="flex-1 internal">
+                <AutoComplete
+                  fullWidth={true}
+                  hintText="Choose Title"
+                  dataSource={dataSource2}
+                  onUpdateInput={this.handleUpdateInput}
+                  floatingLabelText="Title*"
+                />
+              </div>
+              <div className="flex-1 internal">
+                <AutoComplete
+                  fullWidth={true}
+                  filter={AutoComplete.noFilter}
+                  hintText="Choose Name"
+                  openOnFocus={false}
+                  dataSource={dataSource1}
+                  onUpdateInput={this.handleUpdateInput}
+                  floatingLabelText="Name*"
+                />
+              </div>
+              <div className="flex-1 internal">
+                <DatePicker
+                  className="flex-1"
+                  container="inline"
+                  fullWidth={true}
+                  hintText={<span>Select Date </span>}
+                  floatingLabelText="Date*"
+                  defaultDate={this.state.demoDate}
+                  onChange={this.handleChangeDemoDate}
+                  mode="landscape"
+                  firstDayOfWeek={0}
+                  formatDate={date => date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear()}
+                />
+                {/* must use inline style for position on IconButton to override default */}
+                <IconButton className="inline-icon" style={{ position: 'absolute' }}>
+                  <MonthlyCalendarIcon />
+                </IconButton>
+              </div>
             </div>
-        );
-    }
+            <div className="row-spacer-24">
+              <h3>Attachments</h3>
+              <img src="/images/fileUpload.png" />
+            </div>
+            {this.props.children}
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
 }
 
 RiskDecisionCore.defaultProps = defaultProps;
