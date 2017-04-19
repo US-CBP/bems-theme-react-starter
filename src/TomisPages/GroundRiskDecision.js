b5275d63111408320dd0754b4390b109c570609d
Greg <gtfullton@gmail.com>
2017-04-19 06:54:52 -0400

    Failed discovery session on how to apply items to CardHeader.

--- src/TomisPages/GroundRiskDecision.js
+++ src/TomisPages/GroundRiskDecision.js
@@ -15,22 +15,22 @@ import SvgIconArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-do
 import { indigo900 } from 'material-ui/styles/colors';
 
 const initState = {
-    dataSource1: [],
-    dataSource2: [],
-    isExpanded: true,
-    demoDate: new Date(),
-    autoOk: false,
-    disableYearSelection: false
+  dataSource1: [],
+  dataSource2: [],
+  isExpanded: true,
+  demoDate: new Date(),
+  autoOk: false,
+  disableYearSelection: false
 };
 
 const defaultProps = {
-    isAccept: false,
-    isReject: false
+  isAccept: false,
+  isReject: false
 };
 
 const propTypes = {
-    isAccept: PropTypes.bool,
-    isReject: PropTypes.bool
+  isAccept: PropTypes.bool,
+  isReject: PropTypes.bool
 };
 
 const selectedBackgroundColor = 'rgba(0,0,0,0.27)';
@@ -40,70 +40,69 @@ const selectedLabelColor = 'rgba(0,0,0,0.87)';
 const unselectedLabelColor = 'rgba(0,0,0,0.54)';
 
 class GroundRiskDecision extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initState;
-        this.handleUpdateInput = this.handleUpdateInput.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = initState;
+    this.handleUpdateInput = this.handleUpdateInput.bind(this);
+  }
 
-    handleUpdateInput(value) {
-        this.setState({
-            dataSource1: [value, value + value, value + value + value],
-            dataSource2: [value, value + value, value + value + value]
-        });
-    }
+  handleUpdateInput(value) {
+    this.setState({
+      dataSource1: [value, value + value, value + value + value],
+      dataSource2: [value, value + value, value + value + value]
+    });
+  }
 
-    handleChangeDemoDate = (event, date) => {
-        this.setState({
-            demoDate: date
-        });
-    };
+  handleChangeDemoDate = (event, date) => {
+    this.setState({
+      demoDate: date
+    });
+  };
 
-    getBackgroundColorAccept = () => {
-        const { isAccept } = this.props;
-        return isAccept ? selectedBackgroundColor : unselectedBackgroundColor;
-    };
+  getBackgroundColorAccept = () => {
+    const { isAccept } = this.props;
+    return isAccept ? selectedBackgroundColor : unselectedBackgroundColor;
+  };
 
-    getLabelColorAccept = () => {
-        const { isAccept } = this.props;
-        return isAccept ? selectedLabelColor : unselectedLabelColor;
-    };
+  getLabelColorAccept = () => {
+    const { isAccept } = this.props;
+    return isAccept ? selectedLabelColor : unselectedLabelColor;
+  };
 
-    getBackgroundColorReject = () => {
-        const { isReject } = this.props;
-        return isReject ? selectedBackgroundColor : unselectedBackgroundColor;
-    };
+  getBackgroundColorReject = () => {
+    const { isReject } = this.props;
+    return isReject ? selectedBackgroundColor : unselectedBackgroundColor;
+  };
 
-    getLabelColorReject = () => {
-        const { isReject } = this.props;
-        return isReject ? selectedLabelColor : unselectedLabelColor;
-    };
+  getLabelColorReject = () => {
+    const { isReject } = this.props;
+    return isReject ? selectedLabelColor : unselectedLabelColor;
+  };
 
-    render() {
-        const { getBackgroundColorAccept, getLabelColorAccept, getBackgroundColorReject, getLabelColorReject } = this;
-        const { isAccept, isReject } = this.props;
-        const { dataSource1, dataSource2 } = this.state;
-        return (
-            <div>
-                <HeaderNavAction actionBarPageTitle="Ground Operation" />
-                <Card expanded={true}>
-                    <CardHeader title="Risk Decision"  actAsExpander={true} showExpandableButton={true} />
-                    <CardText expandable={true}>
-                        <div><u>Link to Sharepoint Site</u></div><br />
-                        <div className="flex-row">
-                            <div>
-                                <label className="labeled-item">Ground Operation Status (RA)*</label>
-                                <RaisedButton label="Executed" backgroundColor={getBackgroundColorAccept()} labelColor={getLabelColorAccept()} />
-                                <RaisedButton label="Canceled" backgroundColor={getBackgroundColorReject()} labelColor={getLabelColorReject()} />
-                            </div>
+  render() {
+    const { getBackgroundColorAccept, getLabelColorAccept, getBackgroundColorReject, getLabelColorReject } = this;
+    const { isAccept, isReject } = this.props;
+    const { dataSource1, dataSource2 } = this.state;
+    return (
+      <div>
+        <HeaderNavAction actionBarPageTitle="Ground Operation" />
+        <Card expanded={true}>
+          <CardHeader title="Risk Decision" actAsExpander={true} showExpandableButton={true} />
+          <CardText expandable={true}>
+            <div><a className="panel-link">Link to Sharepoint Site</a></div><br />
+            <div className="flex-row">
+              <div>
+                <label className="labeled-item">Ground Operation Status (RA)*</label>
+                <RaisedButton label="Executed" backgroundColor={getBackgroundColorAccept()} labelColor={getLabelColorAccept()} />
+                <RaisedButton label="Canceled" backgroundColor={getBackgroundColorReject()} labelColor={getLabelColorReject()} />
+              </div>
 
-
-                        </div>
-                    </CardText>
-                </Card>
             </div>
-        );
-    }
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
 }
 
 GroundRiskDecision.defaultProps = defaultProps;
