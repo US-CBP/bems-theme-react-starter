0fdbcf1f7752a3e7769a94a1442bc1a629069aa5
Greg <gtfullton@gmail.com>
2017-07-28 22:11:28 -0400

    Implement react-select Input

--- src/BemsMui/AutoComplete.js
+++ src/BemsMui/AutoComplete.js
@@ -17,223 +17,266 @@ import AutoCompleteSelect from './AutoCompleteSelect';
 import InputLabel from 'material-ui/Input/InputLabel';
 import FormControl from 'material-ui/Form/FormControl';
 import FormHelperText from 'material-ui/Form/FormHelperText';
+import _get from 'lodash/get';
 
 const suggestions = [
-    { label: 'Afghanistan' },
-    { label: 'Aland Islands' },
-    { label: 'Albania' },
-    { label: 'Algeria' },
-    { label: 'American Samoa' },
-    { label: 'Andorra' },
-    { label: 'Angola' },
-    { label: 'Anguilla' },
-    { label: 'Antarctica' },
-    { label: 'Antigua and Barbuda' },
-    { label: 'Argentina' },
-    { label: 'Armenia' },
-    { label: 'Aruba' },
-    { label: 'Australia' },
-    { label: 'Austria' },
-    { label: 'Azerbaijan' },
-    { label: 'Bahamas' },
-    { label: 'Bahrain' },
-    { label: 'Bangladesh' },
-    { label: 'Barbados' },
-    { label: 'Belarus' },
-    { label: 'Belgium' },
-    { label: 'Belize' },
-    { label: 'Benin' },
-    { label: 'Bermuda' },
-    { label: 'Bhutan' },
-    { label: 'Bolivia, Plurinational State of' },
-    { label: 'Bonaire, Sint Eustatius and Saba' },
-    { label: 'Bosnia and Herzegovina' },
-    { label: 'Botswana' },
-    { label: 'Bouvet Island' },
-    { label: 'Brazil' },
-    { label: 'British Indian Ocean Territory' },
-    { label: 'Brunei Darussalam' }
+  { label: 'Afghanistan' },
+  { label: 'Aland Islands' },
+  { label: 'Albania' },
+  { label: 'Algeria' },
+  { label: 'American Samoa' },
+  { label: 'Andorra' },
+  { label: 'Angola' },
+  { label: 'Anguilla' },
+  { label: 'Antarctica' },
+  { label: 'Antigua and Barbuda' },
+  { label: 'Argentina' },
+  { label: 'Armenia' },
+  { label: 'Aruba' },
+  { label: 'Australia' },
+  { label: 'Austria' },
+  { label: 'Azerbaijan' },
+  { label: 'Bahamas' },
+  { label: 'Bahrain' },
+  { label: 'Bangladesh' },
+  { label: 'Barbados' },
+  { label: 'Belarus' },
+  { label: 'Belgium' },
+  { label: 'Belize' },
+  { label: 'Benin' },
+  { label: 'Bermuda' },
+  { label: 'Bhutan' },
+  { label: 'Bolivia, Plurinational State of' },
+  { label: 'Bonaire, Sint Eustatius and Saba' },
+  { label: 'Bosnia and Herzegovina' },
+  { label: 'Botswana' },
+  { label: 'Bouvet Island' },
+  { label: 'Brazil' },
+  { label: 'British Indian Ocean Territory' },
+  { label: 'Brunei Darussalam' }
 ];
 
 function renderInputORIGINAL(inputProps) {
-    const { classes, home, value, ref, ...other } = inputProps;
-    return (
-        <TextField
-            autoFocus={home}
-            className={classes.textField}
-            value={value}
-            inputRef={ref}
-            InputProps={{
-                classes: {
-                    input: classes.input
-                },
-                ...other
-            }}
-        />
-    );
+  const { classes, home, value, ref, ...other } = inputProps;
+  return (
+    <TextField
+      autoFocus={home}
+      className={classes.textField}
+      value={value}
+      inputRef={ref}
+      InputProps={{
+        classes: {
+          input: classes.input
+        },
+        ...other
+      }}
+    />
+  );
 }
 
 function renderInput(lov, allInputProps) {
-    const { classes, home, value, ref, ...other } = allInputProps;
-    const inputProps = { classes: { input: classes.input }, lov: lov, ...other };
-    return (
-        <Input disabled={false} component={AutoCompleteInput} fullWidth={true} autoFocus={home} classes={classes.input} value={value} inputRef={ref} inputProps={{ inputProps }} />
-    );
+  const { classes, home, value, ref, ...other } = allInputProps;
+  const inputProps = { classes: { input: classes.input }, lov: lov, ...other };
+  return (
+    <Input
+      disabled={false}
+      component={AutoCompleteInput}
+      fullWidth={true}
+      autoFocus={home}
+      classes={classes.input}
+      value={value}
+      inputRef={ref}
+      inputProps={{ inputProps }}
+    />
+  );
 }
 
 function renderSuggestion(suggestion, { query, isHighlighted }) {
-    const matches = match(suggestion.label, query);
-    const parts = parse(suggestion.label, matches);
+  const matches = match(suggestion.label, query);
+  const parts = parse(suggestion.label, matches);
 
-    return (
-        <MenuItem selected={isHighlighted} component="div">
-            <div>
-                {parts.map((part, index) => {
-                    return part.highlight
-                        ? <span key={index} style={{ fontWeight: 300 }}>
-                              {part.text}
-                          </span>
-                        : <strong key={index} style={{ fontWeight: 500 }}>
-                              {part.text}
-                          </strong>;
-                })}
-            </div>
-        </MenuItem>
-    );
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight
+            ? <span key={index} style={{ fontWeight: 300 }}>
+                {part.text}
+              </span>
+            : <strong key={index} style={{ fontWeight: 500 }}>
+                {part.text}
+              </strong>;
+        })}
+      </div>
+    </MenuItem>
+  );
 }
 
 function renderSuggestionsContainer(options) {
-    const { containerProps, children } = options;
+  const { containerProps, children } = options;
 
-    return (
-        <Paper {...containerProps} square>
-            {children}
-        </Paper>
-    );
+  return (
+    <Paper {...containerProps} square>
+      {children}
+    </Paper>
+  );
 }
 
 function getSuggestionValue(suggestion) {
-    return suggestion.label;
+  return suggestion.label;
 }
 
 function getSuggestions(value) {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-    let count = 0;
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
 
-    return inputLength === 0
-        ? suggestions
-        : suggestions.filter(suggestion => {
-              const keep = count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
+  return inputLength === 0
+    ? suggestions
+    : suggestions.filter(suggestion => {
+        const keep = count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
 
-              if (keep) {
-                  count += 1;
-              }
+        if (keep) {
+          count += 1;
+        }
 
-              return keep;
-          });
+        return keep;
+      });
 }
 
 const styleSheet = createStyleSheet('AutoComplete', theme => ({
-    container: {
-        flexGrow: 1,
-        position: 'relative',
-        height: 200
-    },
-    suggestionsContainerOpen: {
-        position: 'absolute',
-        marginTop: theme.spacing.unit,
-        marginBottom: theme.spacing.unit * 3,
-        left: 0,
-        right: 0
-    },
-    suggestion: {
-        display: 'block'
-    },
-    suggestionsList: {
-        margin: 0,
-        padding: 0,
-        listStyleType: 'none'
-    },
-    textField: {
-        width: '100%'
-    }
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+    height: 200
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit * 3,
+    left: 0,
+    right: 0
+  },
+  suggestion: {
+    display: 'block'
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none'
+  },
+  textField: {
+    width: '100%'
+  }
 }));
 
+const options = [{ value: 'one', label: 'One' }, { value: 'two', label: 'Two' }];
+
 class AutoComplete extends Component {
-    state = {
-        value: '',
-        suggestions: [suggestions]
-    };
+  state = {
+    value: '',
+    suggestions: [suggestions],
+    name: null,
+    val: null
+  };
 
-    lov = null;
+  lov = null;
 
-    handleSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: getSuggestions(value)
-        });
-    };
+  handleSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
 
-    handleSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: []
-        });
-    };
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
-    handleChange = (event, { newValue }) => {
-        this.setState({
-            value: newValue
-        });
-    };
+  handleChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
 
-    shouldRenderSuggestions = val => {
-        return true;
-    };
+  shouldRenderSuggestions = val => {
+    return true;
+  };
 
-    render() {
-        const { shouldRenderSuggestions } = this;
-        const { classes } = this.props;
+  handleInputChange = val => {
+    console.log('handleInputChange val=', val);
+    this.setState({ name: _get(val, 'label', null), val: _get(val, 'value', null) });
+  };
 
-        return (
-            <div>
-                <FormControl>
-                    <InputLabel htmlFor="name">Cloneable</InputLabel>
-                    <Input id="name" component={AutoCompleteSelect} value="" />
-                    <FormHelperText>Some important helper text</FormHelperText>
-                </FormControl>
-                <Autosuggest
-                    ref={ref => {
-                        this.lov = ref;
-                    }}
-                    theme={{
-                        container: classes.container,
-                        suggestionsContainerOpen: classes.suggestionsContainerOpen,
-                        suggestionsList: classes.suggestionsList,
-                        suggestion: classes.suggestion
-                    }}
-                    renderInputComponent={renderInput.bind(this, this.lov)}
-                    suggestions={this.state.suggestions}
-                    onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
-                    renderSuggestionsContainer={renderSuggestionsContainer}
-                    getSuggestionValue={getSuggestionValue}
-                    renderSuggestion={renderSuggestion}
-                    shouldRenderSuggestions={shouldRenderSuggestions}
-                    inputProps={{
-                        autoFocus: false,
-                        classes,
-                        placeholder: 'Search a country (start with a)',
-                        value: this.state.value,
-                        onChange: this.handleChange
-                    }}
-                />
-            </div>
-        );
-    }
+  render() {
+    const { shouldRenderSuggestions, handleInputChange } = this;
+    const { name, val } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <div>
+        {/* <FormControl>
+          <InputLabel htmlFor="name">Cloneable</InputLabel>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Input id="name" component={AutoCompleteSelect} value="" />
+          </div>
+          <FormHelperText>Some important helper text</FormHelperText>
+        </FormControl> */}
+
+        <TextField
+          id="nameField"
+          label="Name Field"
+          labelClassName="cloneable-floating-label"
+          className={classes.textField}
+          value={name}
+          inputProps={{
+            options,
+            value: val,
+            placeholder: 'Select Name Field'
+          }}
+          InputProps={{
+            component: AutoCompleteSelect,
+            onChange: handleInputChange
+          }}
+          margin="dense"
+          helperText="Select a number"
+          disabled={false}
+        />
+
+        {/* <Autosuggest
+          ref={ref => {
+            this.lov = ref;
+          }}
+          theme={{
+            container: classes.container,
+            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion
+          }}
+          renderInputComponent={renderInput.bind(this, this.lov)}
+          suggestions={this.state.suggestions}
+          onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
+          renderSuggestionsContainer={renderSuggestionsContainer}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          shouldRenderSuggestions={shouldRenderSuggestions}
+          inputProps={{
+            autoFocus: false,
+            classes,
+            placeholder: 'Search a country (start with a)',
+            value: this.state.value,
+            onChange: this.handleChange
+          }}
+        /> */}
+      </div>
+    );
+  }
 }
 
 AutoComplete.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styleSheet)(AutoComplete);
--- src/BemsMui/AutoCompleteSelect.js
+++ src/BemsMui/AutoCompleteSelect.js
@@ -1,82 +1,20 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 import FontIcon from './FontIcon';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import Input from 'material-ui/Input';
-import InputLabel from 'material-ui/Input/InputLabel';
-
-const options = [{ value: 'one', label: 'One' }, { value: 'two', label: 'Two' }];
-
-const defaultProps = {
-    id: `ACI-${new Date().getTime()}`
-};
 
 class AutoCompleteSelect extends Component {
-    input = null;
-    isFocused = false;
-
-    state = {
-        val: ''
-    };
-
-    focus = () => {
-        this.input.focus();
-        this.isFocused = true;
-    };
-
-    blur = () => {
-        this.input.blur();
-        this.isFocused = false;
-    };
-
-    handleChangeLovVal = val => {
-        this.setState({ val });
-    };
-
-    //   handleBlur = () => {
-    //     this.isFocused = false;
-    //   };
-
-    //   handleFocus = () => {
-    //     this.isFocused = true;
-    //   };
-
-    handleClickArrowDropDown = evt => {
-        const { inputProps: { lov, lov: { state: lovState, input } } } = this.props;
-        // console.log('handleClickArrowDropDown, lov.input, lov.state=', lov.input, lov.state);
-        console.log('handleClickArrowDropDown, lov.state=', lov.state);
-        const { isFocused } = lovState;
-        if (isFocused) {
-            input.blur();
-        } else {
-            input.focus();
-        }
-    };
-
-    // handleClickArrowDropDown = evt => {
-    //     const { input, isFocused } = this;
-    //     if (isFocused) {
-    //         input.blur();
-    //     } else {
-    //         input.focus();
-    //     }
-    // };
-
-    render() {
-        const { handleChangeLovVal } = this;
-        const { val } = this.state;
-        const classes = this.props.classes;
-        //this.props originate from MUI <Input /> Component
-        //inputProps originate from AutoSuggest Component
-        return (
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <FontIcon name="arrow_drop_down" />
-                <Select options={options} onChange={handleChangeLovVal} value={val} clearable={false} />
-            </div>
-        );
-    }
+  render() {
+    const { value } = this.props;
+    const { disabled, onChange: handleChangeLovVal, options, placeholder } = this.props;
+    const displayPlaceholder = disabled ? '' : placeholder;
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <FontIcon name="arrow_drop_down" />
+        <Select options={options} disabled={disabled} placeholder={displayPlaceholder} onChange={handleChangeLovVal} value={value} clearable={false} />
+      </div>
+    );
+  }
 }
 
-AutoCompleteSelect.defaultProps = defaultProps;
 export default AutoCompleteSelect;
