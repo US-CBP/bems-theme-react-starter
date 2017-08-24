4c9d52a52c174231875e0f86c8f2181e5d2c17cb
Greg <gtfullton@gmail.com>
2017-08-24 11:23:26 -0400

    Old way of performing module.exports no longer works.

--- src/TomisApp/helpers/autocompleteMenuRenderer.js
+++ src/TomisApp/helpers/autocompleteMenuRenderer.js
@@ -5,51 +5,51 @@ const anchorOrigin = { horizontal: 'left', vertical: 'top' };
 const targetOrigin = { horizontal: 'left', vertical: 'top' };
 
 function menuRenderer({
-    focusedOption,
-    instancePrefix,
-    labelKey,
-    onFocus,
-    onSelect,
-    optionClassName,
-    optionComponent,
-    optionRenderer,
-    options,
-    valueArray,
-    valueKey,
-    onOptionRef
+  focusedOption,
+  instancePrefix,
+  labelKey,
+  onFocus,
+  onSelect,
+  optionClassName,
+  optionComponent,
+  optionRenderer,
+  options,
+  valueArray,
+  valueKey,
+  onOptionRef
 }) {
-    let Option = optionComponent;
+  let Option = optionComponent;
 
-    return options.map((option, i) => {
-        let isSelected = valueArray && valueArray.indexOf(option) > -1;
-        let isFocused = option === focusedOption;
-        let optionClass = classNames(optionClassName, {
-            'Select-option': true,
-            'is-selected': isSelected,
-            'is-focused': isFocused,
-            'is-disabled': option.disabled
-        });
-
-        return (
-            <Option
-                className={optionClass}
-                instancePrefix={instancePrefix}
-                isDisabled={option.disabled}
-                isFocused={isFocused}
-                isSelected={isSelected}
-                key={`option-${i}-${option[valueKey]}`}
-                onFocus={onFocus}
-                onSelect={onSelect}
-                option={option}
-                optionIndex={i}
-                ref={ref => {
-                    onOptionRef(ref, isFocused);
-                }}
-            >
-                {optionRenderer(option, i)}
-            </Option>
-        );
+  return options.map((option, i) => {
+    let isSelected = valueArray && valueArray.indexOf(option) > -1;
+    let isFocused = option === focusedOption;
+    let optionClass = classNames(optionClassName, {
+      'Select-option': true,
+      'is-selected': isSelected,
+      'is-focused': isFocused,
+      'is-disabled': option.disabled
     });
+
+    return (
+      <Option
+        className={optionClass}
+        instancePrefix={instancePrefix}
+        isDisabled={option.disabled}
+        isFocused={isFocused}
+        isSelected={isSelected}
+        key={`option-${i}-${option[valueKey]}`}
+        onFocus={onFocus}
+        onSelect={onSelect}
+        option={option}
+        optionIndex={i}
+        ref={ref => {
+          onOptionRef(ref, isFocused);
+        }}
+      >
+        {optionRenderer(option, i)}
+      </Option>
+    );
+  });
 }
 
-module.exports = menuRenderer;
+export default menuRenderer;
